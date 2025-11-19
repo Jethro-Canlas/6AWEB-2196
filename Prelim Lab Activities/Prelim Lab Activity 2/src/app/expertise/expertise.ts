c16f626bb29656d4017f5ea1bf0e4535cadd3655
jethro-canlas <jethrocanlas7@gmail.com>
2025-11-19 15:20:35 +0800

    .

---
+++ Prelim Lab Activities/Prelim Lab Activity 2/src/app/expertise/expertise.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expertise',
+  imports: [],
+  templateUrl: './expertise.html',
+  styleUrl: './expertise.css',
+})
+export class Expertise {
+
+}
