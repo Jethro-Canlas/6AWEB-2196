fdfd6e4ecf80de0efc0bb7e3fb16e2c3319eead3
jethro-canlas <jethrocanlas7@gmail.com>
2025-11-19 16:05:17 +0800

    feat: Add data binding demo component with styling and tests
    - Created data-binding-demo component with HTML and CSS for highlighting text.
    - Implemented basic unit tests for the data-binding-demo component.
    - Updated main application files including index.html and main.ts for component integration.
    - Configured TypeScript settings for the application and tests.

---
+++ Prelim Lab Activities/Prelim Lab Data Binding Demo/prelim_lab_data_binding/src/app/data-binding-demo/data-binding-demo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding-demo',
+  imports: [],
+  templateUrl: './data-binding-demo.html',
+  styleUrl: './data-binding-demo.css',
+})
+export class DataBindingDemo {
+  isHighlighted = true;
+}
