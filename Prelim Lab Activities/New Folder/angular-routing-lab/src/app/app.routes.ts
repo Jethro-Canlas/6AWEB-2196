d30d23cb441af369523a70733b24a510dac69e8b
jethro-canlas <jethrocanlas7@gmail.com>
2025-12-03 15:17:14 +0800

    netlify

--- Prelim Lab Activities/New Folder/angular-routing-lab/src/app/app.routes.ts
+++ Prelim Lab Activities/New Folder/angular-routing-lab/src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { Pagenotfound } from './pagenotfound/pagenotfound';
 import { DataBinding } from './data-binding/data-binding';
 import { Directives } from './directives/directives';
 import { Home } from './home/home';
@@ -6,5 +7,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'directives', component: Directives },
-  { path: 'data-binding', component: DataBinding }
+  { path: 'data-binding', component: DataBinding },
+  { path: '**', component: Pagenotfound }
 ];
