44dd98ec66ab245856c1c5d3f20524db39e45a98
Jethro-Canlas <jethrocanlas7@gmail.com>
2025-12-07 02:26:13 +0800

    employees

--- Prelim Lab Activities/Prelim Lab Activity 5/prelim-project-group-5/src/app/employee-directory/employee-directory.ts
+++ Prelim Lab Activities/Prelim Lab Activity 5/prelim-project-group-5/src/app/employee-directory/employee-directory.ts
@@ -7,10 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './employee-directory.css',
 })
 export class EmployeeDirectory {
-  phenomeman = '/Phenomeman.webp';
+  blondeblazer = '/Blonde_Blazer.webp';
+  phenomaman = '/Phenomaman.webp';
   sweettalker = '/Sweettalker.webp';
   equilibrium = '/Equilibrium.webp';
   pompom = '/Pom_Pom.webp';
   brickhouse = '/Brickhouse.webp';
   brainbook = '/Brainbook.webp';
+
+  coupe = '/Coupe.webp';
+  flambae = '/Flambae.webp';
+  golem = '/Golem.webp';
+  invisigal = '/Invisigal.webp';
+  malevola = '/Malevola.webp';
+  prism = '/Prism.webp';
+  punchup = '/Punch_Up.webp';
+  sonar = '/Sonar.webp';
 }
