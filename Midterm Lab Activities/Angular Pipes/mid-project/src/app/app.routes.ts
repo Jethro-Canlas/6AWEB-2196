aac7b4ad50b3296b349ba4f3fc528beed81aec6b
Jethro-Canlas <jethrocanlas7@gmail.com>
2026-01-14 15:26:50 +0800

    feat: Implement PipesDemo component with various Angular pipes
    - Added PipesDemo component to showcase Angular pipes including Date, Currency, Slice, Async, Decimal, Percent, Title Case, and Key Value pipes.
    - Created corresponding HTML and CSS files for the PipesDemo component.
    - Updated app routing to include PipesDemo as the main component.
    - Modified app component to import PipesDemo and set it as a standalone component.
    - Added unit tests for PipesDemo component to ensure functionality.

--- Midterm Lab Activities/Angular Pipes/mid-project/src/app/app.routes.ts
+++ Midterm Lab Activities/Angular Pipes/mid-project/src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { PipesDemo } from './pipes-demo/pipes-demo';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: '', component: PipesDemo}
+];
