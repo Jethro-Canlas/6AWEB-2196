5045965e89e32ead9f1d10e1b5cc2f4cb2a825c7
Jethro-Canlas <jethrocanlas7@gmail.com>
2026-02-18 14:16:56 +0800

    forms

---
+++ Finals Lab Activities/angular-forms/src/app/template-demo/template-demo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-demo',
+  imports: [],
+  templateUrl: './template-demo.html',
+  styleUrl: './template-demo.css',
+})
+export class TemplateDemo {
+
+}
